59afd6284a13cd51b6847239a815941dca97a451
Iñaki Hulsman <inaruiz600@gmail.com>
2020-08-12 22:46:23 +0200

    Escena de Personaje Nuevo Desbloqueado

---
+++ Kebabs/Assets/Scripts/Jugadores/NuevoPersonaje.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuevoPersonaje : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+    private float timer;
+    public GameObject Nombre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = transform.GetComponent<SpriteRenderer>();
+        sr.color = Color.black;
+        timer = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer < 5){
+            
+            if (timer > 2)
+            {
+                
+                float t = (timer - 2) / 3;
+                sr.color = Color.Lerp(Color.black, Color.white, t);
+            }
+        }else if(timer > 7)
+        {
+            transform.GetComponent<Animator>().enabled = true;
+            Nombre.SetActive(true);
+        }
+        
+    }
+}
